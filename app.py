972864596bb8a2619fd2159db54d9158e9e4999c
JulioCesar10-git <juliocece10@icloud.com>
2026-06-16 10:46:47 -0600

    main para probar libro_dao

---
+++ app.py
@@ -0,0 +1,31 @@
+from dao.libro_dao import LibroDAO
+from models.libro import libro
+
+def main():
+    try:    
+        libro_dao = LibroDAO()
+
+        libros = libro_dao.obtener_todos()
+        
+        print("=== Libros en la biblioteca ===")
+
+        if len(libros) == 0:
+            print("No hay libros registrados.")
+        else:
+            for libros in libros:
+
+                print("------------------------") 
+                print(
+                    f"ID: {libro.id}, Titulo: {libro.titulo}, "
+                    f"Autor {libro.autor}, ISBN: {libro.isbn}, " 
+                    f"Disponible: {'Si' if libro.disponible else 'No'}"
+                )
+                print("------------------------")
+        print("\n Conexion exitosa a la base de datos")
+    except Exception as e:
+
+        print("Error: ")
+        print (e)
+
+if __name__ == "__main__":
+    main()
