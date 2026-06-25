87bd033db709e85b5165516b27b354c4216ead53
JulioCesar10-git <juliocece10@icloud.com>
2026-06-25 11:56:08 -0600

    Actualizar libro

--- app.py
+++ app.py
@@ -2,29 +2,22 @@ from dao.libro_dao import LibroDAO
 from models.libro import Libro
 
 def ver_libros():
-    try:    
+    try:
         libro_dao = LibroDAO()
         libros = libro_dao.obtener_todos()
-        
-        print("=========== Libros en la biblioteca ===========")
 
         if len(libros) == 0:
-            print("No hay libros registrados.")
+            print("No hay libros registrados")
         else:
             for libro in libros:
+                print("================================================================================")
+                print(f"id: {libro.id}, {libro.titulo}, {libro.autor}, {libro.isbn}, {libro.disponible}")
+                print("================================================================================")
+        print(f"\n Conexion exitosa con la base de datos")
 
-                print("__________________________________________________________________") 
-                print(
-                    f"ID: {libro.id}, Titulo: {libro.titulo}, "
-                    f"Autor: {libro.autor}, ISBN: {libro.isbn}, " 
-                    f"Disponible: {'Si' if libro.disponible else 'No'}"
-                )
-                print("__________________________________________________________________") 
-        print("\n Conexion exitosa a la base de datos")
     except Exception as e:
-
-        print("Error: ")
-        print (e)
+        print("Error")            
+        print(e)
 
 def insertar_libro():
     titulo = input("Escribe el titulo del nuevo libro: ")
@@ -47,17 +40,21 @@ def insertar_libro():
 
 def actualizar_libro():
     try:
-        libro_dao =LibroDAO()  
+        libro_dao = LibroDAO()  
+
         print("Lista de libros disponibles")  
         ver_libros() 
-        id = int(input("Seleccione el id de el libro a actualizar"))   
+
+        id = int(input("Seleccione el id de el libro a actualizar: "))   
         titulo = input("Escribe el titulo: ")
         autor = int(input("Escribe el id del autor: "))
         isbn = input("Escribe el isbn: ")
         disponible = bool(input("Escribe si esta disponible: "))
         libro = Libro(id, titulo, autor, isbn, disponible)
+
         libro_dao.actualizar(libro)
-        print("El libro fue actualizado con exito")
+        print(f"El libro {id} fue actualizado con exito")
+
     except Exception as e:
         print("Error al actualizar el libro")    
         print(e)
@@ -67,22 +64,24 @@ def eliminar_libro():
    
     try:
         Libro_dao = LibroDAO()
+        
         print("Lista de libros disponibles")
         ver_libros()
+
         id = int(input("Escriba el id de el libro a eliminar: "))
         Libro_dao.eliminar(id)
+
         print(f"El libro {id} ha sido eliminado con exito")
+
     except Exception as e:
         print(f"Error al eliminar el libro {id}")   
         print(e)     
 
-def main():
-
-    print("======== BIBLIOTECA UNIVERSITARIA ========")
+def menu_libros():
     print("1. Ver todos los libros")
     print("2. Insertar un nuevo libro")
-    print("3. Actualizar un libro disponible")
-    print("4. Eliminar un libro disponible")
+    print("3. Actualizar un libro existente")
+    print("4. Eliminar un libro existente")
     opcion = int(input("Selecciona una opcion (1-4): "))
 
     match opcion:
@@ -93,7 +92,126 @@ def main():
         case 3:
             actualizar_libro()
         case 4:
-            eliminar_libro()        
+            eliminar_libro()     
+
+
+def ver_usuarios():
+    try:
+        usuario_dao = usuarioDAO()
+        usuarios = usuario_dao.obtener_todo()
+
+        if len(usuarios) == 0:
+            print("No hay usuarios registrados")
+        else:
+            for usuario in usuarios:
+                print(f"id: {usuario.id} {usuario.nombre} {usuario.matricula} {usuario.carrera} {usuario.correo} {usuario.activo}")
+                print(f"\n Conexion exitosa con la base de datos")
+
+    except Exception as e:
+        print("Error")            
+        print(e)
+    try:
+        usuario_dao = LibroDAO()
+        usuarios = usuario_dao.obtener_todo()
+
+        if len(usuarios) == 0:
+            print("No hay usuarios registrados")
+        else:
+            for libro in usuarios:
+                print(f"id: {usuario.id} {usuario.nombre} {usuario.matricula} {usuario.carrera} {usuario.correo} {usuario.activo}")
+                print(f"\n Conexion exitosa con la base de datos")
+
+    except Exception as e:
+        print("Error")            
+        print(e)         
+
+def insertar_usuario():
+    nombre = input("Escribe un nuevo usuario: ")
+    matricula = int(input("Escribe la matricula: "))
+    carrera = input("Escribe la carrera: ")
+    correo = input("Escribe el correo: ")
+    activo = True
+
+    try:
+        usuario_dao = usuarioDAO()
+        id = usuario_dao.obtener_ultimo_id() + 1 
+
+        usuario = usuario(id, nombre, matricula, carrera, correo, activo)
+        usuario_dao.insertar(usuario)
+        print("Insercion realizada con exito")
+
+    except Exception as e:
+        print("Error al insertar un nuevo usuario")
+        print(e)
+
+def actualizar_usuario():
+    try:
+        usuario_dao = usuarioDAO()  
+
+        print("Lista de usuarios activos")  
+        ver_usuarios() 
+
+        id = int(input("Seleccione el id de el usuario a actualizar"))   
+        nombre = input("Escribe el nombre: ")
+        matricula = int(input("Escribe la matricula: "))
+        carrera = input("Escribe la carrera: ")
+        correo = input("Escribe el correo: ")
+        disponible = bool(input("Escribe si esta activo: "))
+        usuario = usuario(id, nombre, matricula, carrera, correo, disponible)
+
+        usuario_dao.actualizar(usuario)
+        print(f"El usuario {id} fue actualizado con exito")
+
+    except Exception as e:
+        print("Error al actualizar el usuario")    
+        print(e)
+   
+
+def eliminar_usuario():
+   
+    try:
+        usuario_dao = usuarioDAO()
+        
+        print("Lista de usuarios activos")
+        ver_usuarios()
+
+        id = int(input("Escriba el id de el usuario a eliminar: "))
+        usuario_dao.eliminar(id)
+
+        print(f"El usuario {id} ha sido eliminado con exito")
+
+    except Exception as e:
+        print(f"Error al eliminar el usuario {id}")   
+        print(e)
+
+def menu_usuarios():
+    print("1.Ver todos los usuarios")
+    print("2.Insertar un nuevo usuario")
+    print("3. Actualizar un usuario existente")
+    print("4. Eliminar un usuario existente")
+    opcion = int(input("Selecciona un opcion (1-4): "))
+
+    match opcion:
+        case 1:
+            ver_usuarios()
+        case 2:
+            insertar_usuario()    
+        case 3:
+            actualizar_usuario()   
+        case 4:
+            eliminar_usuario()
+
+def main():
+    print("=== BIBLIOTECA UNIVERSITARIA === ") 
+    print("Menu de opciones: ")
+    print("1. Libros")
+    print("2. Usuarios")
+    opcion = int(input("Escribe tu opcion: "))
+    match opcion:
+        case 1: menu_libros()
+        case 2: menu_usuarios()
+
+    print("Saliendo del sistema de Biblioteca universitaria ... ")    
             
 if __name__ == "__main__":
     main()
--- dao/usuario_dao.py
+++ dao/usuario_dao.py
@@ -1,71 +1,68 @@
-# DAO: DATA ACCES OBJECT
-# libro_dao: Objeto de acceso a datos de la tabla libro
-
 from database import conexion
 from database.conexion import Conexion
-from models.libro import Libro
+from models.usuario import usuario
 
-class LibroDAO: 
+class usuarioDAO: 
 
-    # SELECT * FROM libro
+    # SELECT * FROM Usuario
     def obtener_todos(self):
         conexion = Conexion.obtener_conexion()
         cursor = conexion.cursor()
 
-        cursor.execute("SELECT * FROM vista_libros")
+        cursor.execute("SELECT * FROM vista_usuario")
         registros = cursor.fetchall()
 
-        libros = []
+        usuarios = []
         for registro in registros:
-            libro = Libro(id = registro[0], titulo = registro[1], autor = registro[2], isbn = registro[3], disponible = registro[4])
-            libros.append(libro)
+            usuario = usuario(id = registro[0], nombre = registro[1], matricula = registro[2], carrera = registro[3], correo = registro[4], activo = registro[5])
+            usuario.append(usuario)
             
         cursor.close()
         conexion.close()
-        return libros
+        return usuarios
     
-    def insertar(self, libro):
+    def insertar(self, usuario):
         conexion = Conexion.obtener_conexion()
         cursor = conexion.cursor()
 
         sql = """
 
-            INSERT INTO libro (id, titulo, autor, isbn, disponible)
+            INSERT INTO usuario (id, nombre, matricula, carrera, correo, activo)
             VALUES (%s, %s, %s, %s, %s)
 
         """
 
         cursor.execute(
-            sql, (libro.id, libro.titulo, libro.autor, libro.isbn, libro.disponible) 
+            sql, (usuario.id, usuario.nombre, usuario.matricula, usuario.carrera, usuario.correo, usuario.activo) 
         )
 
         conexion.commit()
         cursor.close()
         conexion.close()
 
-    def actualizar(self, libro):
+    def actualizar(self, usuario):
         conexion = Conexion.obtener_conexion()
         cursor = conexion.cursor()
 
         sql = """
 
-            UPDATE libro SET titulo = %s, autor = %s, isbn = %s, disponible = %s WHERE id = %s
+            UPDATE usuario SET nombre = %s, matricula = %s, carrera = %s, correo = %s, activo = %s, WHERE id = %
             
         """        
 
         cursor.execute(
-            sql, (libro.titulo, libro.autor, libro.isbn, libro.disponible, libro.id)
+            sql, (usuario.titulo, usuario.autor, usuario.isbn, usuario.disponible, usuario.id)
         )
 
         conexion.commit()
         cursor.close()
         conexion.close()
 
-    def eliminar(self, libro_id):
+    def eliminar(self, usuario_id):
         conexion = Conexion.obtener_conexion()
         cursor = conexion.cursor()
 
-        cursor.execute("DELETE FROM libro WHERE id = %s", (libro_id,)) 
+        cursor.execute("DELETE FROM usuarios WHERE id = %s", (usuario_id,)) 
 
         conexion.commit()
         cursor.close()
@@ -75,7 +72,7 @@ class LibroDAO:
         conexion = Conexion.obtener_conexion()
         cursor = conexion.cursor()
 
-        cursor.execute("SELECT id FROM libro ORDER BY id DESC")
+        cursor.execute("SELECT id FROM usuario ORDER BY id DESC")
         resultado = cursor.fetchone()
 
         cursor.close()
